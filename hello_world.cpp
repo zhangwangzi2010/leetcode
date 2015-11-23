e464f16db0f70aceb0de801f98fbaf445709c05f
zhangwangzi2010 <clarence.zhang@qq.com>
2015-11-23 15:32:52 +0800

    modify hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <stdlib.h>
+#include <unistd.h>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
 	cout<<"Hello, world!"<<endl;
-	system("pause");
 	return 0;
 }
